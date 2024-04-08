bb6fd04f19ba6095e817ef5d45dd7acf3060b3a1
Y-ouch <792888359@qq.com>
2024-04-08 22:36:59 +0800

    fIx : add something

--- 2697.lexicographically-smallest-palindrome.cpp
+++ 2697.lexicographically-smallest-palindrome.cpp
@@ -5,9 +5,27 @@
  * [2697] 字典序最小回文串
  */
 
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
 // @lc code=start
-#include <string>
-using string = std::string;
+
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
